d74b1dcf7c4dae278d62b0fe2159e92379f0e419
RudrakshDev <wis40311@gmail.com>
2025-09-13 11:32:11 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { SearchInterface } from './components/search-interface'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className='min-h-screen bg-background flex items-center justify-center p-4'>
       <SearchInterface />
+      <Analytics />
     </div>
   )
 }
